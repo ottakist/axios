1596e1fc30ff141a7374fbda616c472470aa6e1d
ottakist <102596775+ottakist@users.noreply.github.com>
2023-01-21 20:53:16 +0200

    finished

--- src/axios/custom.js
+++ src/axios/custom.js
@@ -1,2 +1,6 @@
 import axios from "axios";
-const authFetch = axios.create({})
+const authFetch = axios.create({
+  baseURL: 'https://course-api.com',
+  headers:{Accept:'application/json'}
+});
+export default authFetch
--- src/examples/5-custom-instance.js
+++ src/examples/5-custom-instance.js
@@ -1,10 +1,19 @@
+import axios from 'axios';
 import { useEffect } from 'react';
+import authFetch from '../axios/custom'
+
 
 const randomUserUrl = 'https://randomuser.me/api';
 
 const CustomInstance = () => {
   const fetchData = async () => {
-    console.log('custom axios instance');
+  try {
+    const response1 = await authFetch('/react-store-products');
+    const response2 = await axios(randomUserUrl)
+    console.log(response1)
+  } catch (error) {
+    
+  }
   };
 
   useEffect(() => {
